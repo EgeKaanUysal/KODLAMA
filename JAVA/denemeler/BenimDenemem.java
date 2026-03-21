fd3080cc5d1fe064862ff8b1c595c56e03edfbdc
Your Name <your.email@example.com>
2026-03-22 00:36:54 +0300

    her şey oldu

--- JAVA/denemeler/BenimDenemem.java
+++ JAVA/denemeler/BenimDenemem.java
@@ -8,86 +8,94 @@ public class BenimDenemem {
     static BigInteger carpim = BigInteger.ONE;
     static BigInteger carpimartibir;
     static List<BigInteger> sonuc = new ArrayList<>();
-    
+
 
 
     public static void main (String[] args){
         long count=1;
         BigInteger sayi = new BigInteger("1");
-        while (sayi.compareTo(BigInteger.valueOf(100)) < 0){
+        BigInteger sayi2 = new BigInteger("30");
+        while (sayi.compareTo(sayi2) <= 0){
             sayi=sayi.add(BigInteger.valueOf(2));
-            if (sayi.isProbablePrime(40)){
+            if (sayi.isProbablePrime(40)&&sayi.compareTo(sayi2) <= 0){
                 if (count%1==0){
                     System.out.println(sayi);
                 }
                count++;
-                list.add(sayi);           
+                list.add(sayi);
         }
         // BigInteger sayi =new BigInteger("9973");
      }
      System.out.println(BenimDenemem.sonuc);
 
      ispat();
+     BigInteger asılbaba= new BigInteger("1");
+     for (BigInteger zikkim :list){
+        asılbaba=asılbaba.multiply(zikkim);
+     }
+     System.out.println();
+     System.out.println(asılbaba);
+     if (carpim.equals(asılbaba)){
+        System.out.println("eşit");
+     }
+     else{
+        System.out.println("eşit değil");
+     }
      asalMi();
+    }
 
-
-}
-public static void ispat(){
-        
+    public static void ispat(){
         for (BigInteger sayi : list) {
             carpim = carpim.multiply(sayi);
         }
-         carpimartibir=carpim.add(BigInteger.ONE);
+        carpimartibir=carpim.add(BigInteger.ONE);
         System.out.println("Çarpım: " + carpim);
         System.out.println();
         System.out.println("-------------------------------------------");
         System.out.println();
-
         System.out.println("yeni sayı: " + carpimartibir);
     }
 
     public static void asalMi(){
-        boolean flag =carpimartibir.isProbablePrime(40);
-    if(flag){
-        System.out.println("Yeni asal bulundu");
-    }
-    else{
-        asalCarpanlar();
+        boolean flag = carpimartibir.isProbablePrime(40);
+        if(flag){
+            System.out.println("Yeni asal bulundu");
+        }
+        else{
+            asalCarpanlar();
+        }
     }
 
-}
- public static void asalCarpanlar() {
-      
+    public static void asalCarpanlar() {
+        BigInteger n = carpimartibir; // geçici kopya, carpimartibir bozulmuyor
         BigInteger iki = BigInteger.valueOf(2);
 
         // 2'leri ayır
-        while (carpimartibir.mod(iki).equals(BigInteger.ZERO)) {
+        while (n.mod(iki).equals(BigInteger.ZERO)) {
             sonuc.add(iki);
-            carpimartibir = carpimartibir.divide(iki);
+            n = n.divide(iki);
         }
 
         // tek bölenleri dene: 3,5,7,...
         BigInteger i = BigInteger.valueOf(3);
-        while (i.multiply(i).compareTo(carpimartibir) <= 0) {
-            while (carpimartibir.mod(i).equals(BigInteger.ZERO)) {
+        while (i.multiply(i).compareTo(n) <= 0) {
+            while (n.mod(i).equals(BigInteger.ZERO)) {
                 sonuc.add(i);
-                carpimartibir = carpimartibir.divide(i);
-                System.out.println(sonuc);
+                n = n.divide(i);
             }
             i = i.add(iki);
-            
         }
 
         // elde kalan >1 ise asaldır
-        if (carpimartibir.compareTo(BigInteger.ONE) > 0) {
-            sonuc.add(carpimartibir);
+        if (n.compareTo(BigInteger.ONE) > 0) {
+            sonuc.add(n);
         }
-System.out.println("bitti");
-       
+        System.out.println("Asal çarpanlar: " + sonuc);
+        System.out.println("asal çarpanların çarpımı: " + sonuc.stream().reduce(BigInteger.ONE, BigInteger::multiply));
+        System.out.println("bitti");
     }
 
     public static void bulma(){
 
     }
-
 }
--- PYHTON/deneme.py
+++ PYHTON/deneme.py
@@ -1,10 +1,16 @@
-import sys
-x=1
-y=2
+# 
+# import sys
+# x=1
+# y=2
 
 
-sys.set_int_max_str_digits(0)   # limiti kaldırır (veya örn: 100000)
+# sys.set_int_max_str_digits(0)   # limiti kaldırır (veya örn: 100000)
 
-for i in range(1,10100):
-    x=x*i
-    print(x)
+# for i in range(1,10100):
+#     x=x*i
+#     print(x)
+list =[2, 2, 2, 2, 2, 2, 2, 19259, 303907, 42346333, 383337523]
+a=1
+for i in list:
+    a=i*a
+    print(a) 
