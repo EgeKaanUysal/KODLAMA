7038d82836bda795f8fce91425c9c8477bddaf76
Your Name <your.email@example.com>
2026-04-05 22:11:42 +0300

    OutOfMemory

---
+++ JAVA/denemeler/OutOfMemory.java
@@ -0,0 +1,26 @@
+package denemeler;
+
+import java.util.ArrayList;
+
+public class OutOfMemory {
+    long a=8786554;
+    long b=8786554;
+    public static void main(String[] args) {
+        for (int i=0; i<12; i++){
+          Islem();
+        }
+                
+            }
+    
+    static void Islem(){
+        System.out.println("------------");
+        ArrayList<OutOfMemory> list = new ArrayList<>();
+             for (long i=0; i<105000000; i++){
+                             
+                OutOfMemory referans= new OutOfMemory();
+                list.add(referans);
+                if (i%1000000==0){
+                    System.out.println(i);
+                }
+    }
+}}
